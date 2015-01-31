c29181df1e3a79d201fe3c50121a622c8d35eab7
Vivek Dixit <vivekdixit05@gmail.com>
2015-01-31 12:52:11 +0530

    Add labmda samples

--- java8poc/src/com/company/Main.java
+++ java8poc/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
 
 public class Main {
 
@@ -14,7 +15,36 @@ public class Main {
             }
         });
 
-        File[] hiddenFiles1=new File(".").listFiles(File::exists);
+        File[] hiddenFiles1 = new File(".").listFiles(File::exists);
 
+
+        //Iterate items of array.
+        System.out.println("Simple lambda expression.");
+        Arrays.asList("a", "b", "c", "d").forEach(e -> System.out.println(e));
+
+        //Item as type parameter.
+        System.out.println("Lambda with type:.");
+        Arrays.asList("a", "b", "c", "d").forEach((String e) -> System.out.println(e));
+
+        //Lambda with complex body.
+        System.out.println("Lambda with complex code.");
+        Arrays.asList("a", "b", "c", "d").forEach(e -> {
+            System.out.println(e);
+            System.out.println(e);
+        });
+
+        //Lambda with class member access
+        System.out.println("Lambda accessing class variable");
+        String separator = ",";
+        Arrays.asList("a", "b", "c", "d").forEach(e -> System.out.println(e + separator));
+
+        //Lambda with return statement
+        Arrays.asList("a", "x", "b", "e").sort((a, b) -> a.compareTo(b));
+
+        //Sorting with lambda body
+        Arrays.asList("a", "x", "b", "e").sort((a, b) -> {
+            int result = a.compareTo(b);
+            return result;
+        });
     }
 }
